e6ccd2c4f7d8be4dd688dbf027a1a29709458451
liqiang <liqiang0335@qq.com>
2024-07-26 10:29:27 +0800

    Fixed

--- commands/build/index.js
+++ commands/build/index.js
@@ -47,7 +47,7 @@ module.exports = async (ctx) => {
     print("rules", option.module.rules);
   }
 
-  // remove custom property
+  // 删除用户配置中的@开头的配置: 如果不删除会报错
   for (let userKey in option) {
     if (/^@/.test(userKey)) {
       delete option[userKey];
@@ -77,28 +77,75 @@ module.exports = async (ctx) => {
   }
 };
 
-/**
- * ----------------------------------------
- * helpers
- * ----------------------------------------
- */
-function checkframework(buildFilePath) {
-  const filePath = buildFilePath + ".js";
-  console.log("⭕️ filePath: ", filePath);
-  let content;
-  try {
-    content = fs.readFileSync(filePath, "utf-8");
-  } catch (err) {
-    throw new Error(`没有找到入口文件: ${buildFilePath}`);
+
+function getWebpackUserOption(yyconfig, ctx) {
+  if (!yyconfig) {
+    print(`"yy.config.js" Not Found => "yy init" `);
+    return {};
   }
-  if (/import\s+vue/i.test(content)) {
-    return "vue";
+
+  // 合并 common 配置 和 pages 配置
+  const { common: commonOption, pages } = yyconfig;
+  const pageOption = pages[ctx.build] || {};
+  const option = deepmerge.all([commonOption, pageOption, { entry: ctx.entry }]);
+  const buildFilePath = path.join(ctx.cwd, option.entry);
+  const buildFolder = path.dirname(buildFilePath);
+
+  if (!option.entry) {
+    throw new Error(`请配置 'entry' 参数`);
   }
-  if (/import\s+react/i.test(content)) {
-    return "react";
+
+  // 生产环境: 输出目录添加版本号
+  if (option['@outputVersion'] === true) {
+    // 这里不能使用 ctx.isPro
+    if (ctx.env === "pro") {
+      const version = pageOption["@version"]
+      // 清空输出目录
+      if (fs.existsSync(option.output.path)) {
+        fs.rmdirSync(option.output.path, { recursive: true });
+      }
+      option.output.path = path.join(option.output.path, version);
+    }
   }
-  return "common";
+
+
+  // 创建HTML模版
+  if (option["@template"] !== false) {
+    const templateFilePath = path.join(buildFolder, "template.html");
+    if (!fs.existsSync(templateFilePath)) {
+      const source = fs.readFileSync(path.join(__dirname, "../../assets/template.html"));
+      fs.writeFileSync(templateFilePath, source);
+      print("添加模版:template.html");
+    }
+  }
+
+  // 相对路径转换为绝对路径
+  if (option?.resolve?.alias) {
+    for (let key in option.resolve.alias) {
+      const value = option.resolve.alias[key];
+      if (!/^\//.test(value)) {
+        option.resolve.alias[key] = path.join(ctx.cwd, value);
+      }
+    }
+  }
+
+  // add alias for @
+  set(option, "resolve.alias.@", buildFolder);
+  return option;
 }
+
+async function getValidPort({ host, port }) {
+  const url = `http://${host}:${port}`;
+  try {
+    await axios.get(url, { timeout: 3000 });
+    return getValidPort({ host, port: port + 1 });
+  } catch (err) {
+    if (err.code === "ECONNREFUSED") {
+      return port;
+    }
+  }
+}
+
 
 function errorHandler(err, stats) {
   if (err) {
@@ -121,57 +168,21 @@ function errorHandler(err, stats) {
   }
 }
 
-function getWebpackUserOption(yyconfig, ctx) {
-  if (!yyconfig) {
-    print(`"yy.config.js" Not Found => "yy init" `);
-    return {};
-  }
 
-  // merge common-pages setup
-  const { common: commonOption, pages } = yyconfig;
-  const pageOption = pages[ctx.build] || {};
-  const option = deepmerge.all([commonOption, pageOption, { entry: ctx.entry }]);
-  const buildFilePath = path.join(ctx.cwd, option.entry);
-  const buildFolder = path.dirname(buildFilePath);
-
-  if (!option.entry) {
-    throw new Error(`请配置 'entry' 参数`);
-  }
-
-  // 创建HTML模版
-  if (option["@template"] !== false) {
-    const templateFilePath = path.join(buildFolder, "template.html");
-    if (!fs.existsSync(templateFilePath)) {
-      const source = fs.readFileSync(path.join(__dirname, "../../assets/template.html"));
-      fs.writeFileSync(templateFilePath, source);
-      print("添加模版:template.html");
-    }
-  }
-
-  // relatiev path to aboslute path
-  if (option?.resolve?.alias) {
-    for (let key in option.resolve.alias) {
-      const value = option.resolve.alias[key];
-      if (!/^\//.test(value)) {
-        option.resolve.alias[key] = path.join(ctx.cwd, value);
-      }
-    }
-  }
-
-  // add alias for @
-  set(option, "resolve.alias.@", buildFolder);
-
-  return option;
-}
-
-async function getValidPort({ host, port }) {
-  const url = `http://${host}:${port}`;
+/**
+ * ----------------------------------------
+ * helpers
+ * ----------------------------------------
+ */
+function checkframework(buildFilePath) {
+  const filePath = buildFilePath + ".js";
+  let content;
   try {
-    await axios.get(url, { timeout: 3000 });
-    return getValidPort({ host, port: port + 1 });
+    content = fs.readFileSync(filePath, "utf-8");
   } catch (err) {
-    if (err.code === "ECONNREFUSED") {
-      return port;
-    }
+    throw new Error(`没有找到入口文件: ${buildFilePath}`);
   }
+  if (/import\s+vue/i.test(content)) return "vue";
+  if (/import\s+react/i.test(content)) return "react";
+  return "common";
 }
